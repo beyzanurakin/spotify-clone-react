8b3a9d8f49e7a808e1bce4c5e08a68bc7f30a7a5
beyzanurakin <akinbeyzanur@gmail.com>
2022-06-22 15:43:27 +0300

    add server folder

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
